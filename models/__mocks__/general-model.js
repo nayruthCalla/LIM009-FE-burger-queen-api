4f2870341ba36617af38c5adea80253c856a228e
nayruthCalla <nayruth.calla@gmail.com>
2019-07-17 22:09:04 -0500

    merge test

--- models/__mocks__/general-model.js
+++ models/__mocks__/general-model.js
@@ -3,12 +3,21 @@ const { ObjectId } = require('mongodb');
 const id = { _id: new ObjectId('5d2b074c8d949249fa60e5fe') };
 module.exports = jest.fn((collection, dbUrl) => ({
   createDocument: jest.fn().mockImplementation(() => Promise.resolve({
-    _id: id, email: 'maria@gmail.com', password: '$2b$10$nod2eh0Lq0iPdp0BWLjgpeAt0sXV0Up7cfchL0gK2TZo3VJCgmwAa', roles: { admin: false },
+    ops: [{
+      _id: '123', email: 'meseroMock@gmail.com', password: '$2b$10$nod2eh0Lq0iPdp0BWLjgpeAt0sXV0Up7cfchL0gK2TZo3VJCgmwAa', roles: { admin: false },
+    }],
   })),
   updateDocument: jest.fn().mockImplementation(() => Promise.resolve({ email: 'nayruthupdate@gmail.com', password: '$2b$10$nod2eh0Lq0iPdp0BWLjgpeAt0sXV0Up7cfchL0gK2TZo3VJCgmwAa', roles: { admin: true } })),
   deleteDocument: jest.fn().mockImplementation(() => Promise.resolve({})),
-  searchDataBase: jest.fn().mockImplementation(() => Promise.resolve({
-    _id: id, email: 'maria@gmail.com', password: '$2b$10$nod2eh0Lq0iPdp0BWLjgpeAt0sXV0Up7cfchL0gK2TZo3VJCgmwAa', roles: { admin: false },
-  })),
+  searchDataBase: jest.fn().mockImplementation((doc) => {
+    if (doc.email === 'email already exists') {
+      // console.log('email existe')
+      return Promise.resolve({
+        _id: '123', email: 'meseroMock@gmail.com', password: '$2b$10$nod2eh0Lq0iPdp0BWLjgpeAt0sXV0Up7cfchL0gK2TZo3VJCgmwAa', roles: { admin: false },
+      });
+    }
+    // console.log('null')
+    return Promise.resolve(null);
+  }),
   showListCollections: jest.fn().mockImplementation(() => Promise.resolve({})),
 }));
