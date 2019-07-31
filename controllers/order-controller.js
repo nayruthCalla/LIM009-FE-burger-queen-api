04a6da2b4652035725676953840958a13f8f9473
nayruthCalla <nayruth.calla@gmail.com>
2019-07-31 18:09:38 -0500

    captando error orders

--- controllers/order-controller.js
+++ controllers/order-controller.js
@@ -8,11 +8,16 @@ module.exports = (orderModel, productModel) => ({
     if (!userId || !products) {
       return next(400);
     }
-    const arrayProducts = products.map(async (element) => {
-      const productId = element.product;
-      const ObjProduct = await productModel.searchDataBase({ _id: new ObjectId(productId) });
-      return { qty: element.qty, product: { productId: ObjProduct._id, name: ObjProduct.name, price: ObjProduct.price } };
-    });
+    let arrayProducts;
+    try {
+      arrayProducts = products.map(async (element) => {
+        const productId = element.product;
+        const ObjProduct = await productModel.searchDataBase({ _id: new ObjectId(productId) });
+        return { qty: element.qty, product: { productId: ObjProduct._id, name: ObjProduct.name, price: ObjProduct.price } };
+      });
+    } catch (err) {
+      return resp.send(404);
+    }
     const newOrder = await orderModel.createDocument({
       userId, client, products: await Promise.all(arrayProducts), status: 'pending', dateEntry: new Date(),
     });
