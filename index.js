7a5020deec6bd0fd848f7d3b82fa3c072121f097
nayruthCalla <nayruth.calla@gmail.com>
2019-08-01 19:09:33 -0500

    app

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const config = require('./config');
 const authMiddleware = require('./middleware/auth');
 const errorHandler = require('./middleware/error');
 const routes = require('./routes');
 const pkg = require('./package.json');
 const db = require('./services/connection');
-const cors = require('cors');
 
 const { dbUrl, port, secret } = config;
 const app = express();
-
+app.use(cors());
 // TODO: Conección a la BD en mogodb
 
 db(dbUrl)
@@ -19,8 +19,7 @@ db(dbUrl)
 
     // parse application/x-www-form-urlencoded
     app.use(express.urlencoded({ extended: false }));
-    app.use(express.json());
-    app.use(cors());
+    app.use(express.json());    
     app.use(authMiddleware(secret));
     // Registrar rutas
     routes(app, (err) => {
